b7679d21bfaa2da5afdf19746302089413a49fb2
Carey Ogola <ogolacarey254@gmail.com>
2025-04-01 17:11:04 +0300

    backend-d-01

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const Joi = require('joi');
+
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { name } = require('ejs');
 const db = require("better-sqlite3")("ourApp.db");
 db.pragma("journal_mode = WAL");
+
+
+
 // DB SETUP
 
 const createTables = db.transaction(()=>{
@@ -22,6 +28,13 @@ createTables()
 
 //DB ENDS
 const app = express(); //INITIALISATION
+
+courses = [
+    {id:1, name:"Mathematics"},
+    { id:2, name:"English"},
+    {id:3, name:"Chemistry"}
+]
+app.use(express.json());
 
 app.use(function (req, res, next){
     res.locals.errors = [];
@@ -44,9 +57,29 @@ app.get("/", (req, res)=>{
     
     res.render("home");
 })
-
+app.get("/signup", (req, res)=>{
+    res.status(200).send("signup");
+})
 app.get("/login", (req, res)=>{
     res.render("login");
+})
+app.get('/api/courses', (req, res)=>{
+    res.send(courses);
+})
+
+app.get("/api/courses/:id", (req, res)=>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send("Course not found");
+    res.send(course);
+})
+
+app.post('/api/courses', (req, res)=>{
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
 })
 
 app.post("/register", (req, res)=>{
@@ -63,8 +96,12 @@ app.post("/register", (req, res)=>{
     if(req.body.name && req.body.name.length < 3) errors.push("Name must be at least 3 characters");
     if(req.body.name && req.body.name.length > 10) errors.push("Name cannot be more than 10 characters");
     
+    if(!req.body.password) errors.push("You must provide a password");
+    if(!req.body.confirm_password) errors.push("You must provide a password");
     if(req.body.password && req.body.password.length < 5) errors.push("Password cannot be less than 5 characters");
     if(req.body.password && req.body.password.length > 12) errors.push("Pasword shouldn't exceed 12 characters");
+
+    if(req.body.confirm_password !== req.body.password) errors.push("Passwords do not match");
 
     if(errors.length){
         return res.render("home",{errors})
@@ -90,4 +127,6 @@ app.post("/register", (req, res)=>{
     
     res.send("Thank You!")
 })
-app.listen(3002);
+
+const port = process.env.PORT || 3002;
+app.listen(port);

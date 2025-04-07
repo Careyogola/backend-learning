0a08b105499cf364fdf0206e5089f2f15e90a59a
Carey Ogola <ogolacarey254@gmail.com>
2025-04-07 10:31:41 +0300

    day-04

--- config/mongodb.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-    mongoose.connection.on('connected', () => {
-        console.log("Database is connected!");
-    })
-    await mongoose.connect(`${process.env.MONGO_URI}`);
-}
-export default connectDB;
--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-import 'dotenv/config';
-import express from 'express';
-import connectDB from './config/mongodb.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+const express = require('express');
 const app = express();
 
 
-
-app.get("/", (req, res)=>{
-    res.send("Backend server is working!")
+app.get('/', (req, res)=>{
+    res.send('Hello GearTickets Backend !')
 })
-const port = process.env.PORT || 3003 ;
-app.listen(port)
-connectDB();
+
+app.listen(4000);

e8df767194e6c3c4ac9f235b3e42e793bb135557
Carey Ogola <ogolacarey254@gmail.com>
2025-04-19 18:21:56 +0300

    backend development

---
+++ routes/AuthRoutes.js
@@ -0,0 +1,7 @@
+import express, { Router } from 'express';
+
+
+Router.post('/api/auth/signin', signin);
+
+
+export default Router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv'
+
 const app = express();
 
+dotenv.config();
 
-app.get('/', (req, res)=>{
-    res.send('Hello GearTickets Backend !')
-})
 
-app.listen(4000);
+const port = process.env.PORT || 4000
+
+app.listen(port);

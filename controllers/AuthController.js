196427f038c24ea877f2dba72c00e305d0a3625a
Carey Ogola <ogolacarey254@gmail.com>
2025-04-22 15:38:20 +0300

    login endpoint tested and working

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res) => {
     try {
@@ -37,6 +38,50 @@ export const signup = async (req, res) => {
   
 
 export const signin = async (req, res) => {
+
+    const JWT_SECRET = process.env.JWT_SECRET;
+    try {
+        const { email, password } = req.body;
+
+        //validating inputs
+        if(!email || !password) {
+            return res.status(401).json({ error: "Email and Password is required."})
+        }
+
+          // Find user by email
+  User.findByEmail(email, async (err, results) => {
+    if (err) return res.status(500).json({ error: 'Database error.' });
+    if (results.length === 0) {
+      return res.status(401).json({ error: 'Invalid credentials.' });
+    }
+
+    const user = results[0];
+
+    // Compare passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: 'Invalid credentials.' });
+    }
+
+    // Generate token (optional)
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      JWT_SECRET,
+      { expiresIn: '1d' }
+    );
+
+    res.status(200).json({
+      message: 'Login successful',
+      user: { id: user.id, name: user.name, email: user.email },
+      token
+    });
+  });
+
+
+
+    } catch (error) {
+        
+    }
 
 }
 
